fe6be541d63ced786d53f4d7a8b4460aa25f48af
Arjun S <84306434+ArjunS2001@users.noreply.github.com>
2021-05-20 12:46:39 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,61 @@
+const gorilla = document.getElementById("gorilla");
+const rocks = document.getElementById("rocks");
+const score = document.getElementById("score");
+function jump(){
+  gorilla.classList.add("jump-animation");  
+  setTimeout(() => {
+    gorilla.classList.remove("jump-animation");
+  },500);
+ }
+ document.addEventListener("keypress",() => {
+   if(!gorilla.classList.contains('jump-animation')){
+     jump();
+   }
+  });
+
+// setInterval(function() {
+  setInterval(() => { 
+    //update score
+    score.innerText++;
+    //gorilla x position
+  const gorillatop = parseInt(window.getComputedStyle(gorilla).getPropertyValue("top"));
+   //get current rock y position
+  const rockleft = parseInt(window.getComputedStyle(rock).getPropertyValue("left"));
+    if(rockleft < 0){
+      rock.style.display = 'none';
+    }else{
+      rock.style.display = '';
+    }  
+  //detect collision
+  if(rockleft < 50 && rockleft > 0 && gorillatop > 100){
+    //collision
+    alert("GAME OVER! \n\n You got a score of : "+  score.innerText + "\n\n Press Enter to Play Again");
+    location.reload(); 
+    
+  }
+
+},50);
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
